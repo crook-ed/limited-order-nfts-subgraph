fcc3526a1021d6479b3677738546ecff36397a7b
Croooook <rajatgoyal6404@gmail.com>
2022-03-05 18:05:12 +0530

    reinitialsed aqgain

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,50 +1,31 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract, LogClose, LogCreate } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import {
+  LogClose as LogCloseEvent,
+  LogCreate as LogCreateEvent
+} from "../generated/Contract/Contract"
+import { LogClose, LogCreate } from "../generated/schema"
 
-export function handleLogClose(event: LogClose): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
+export function handleLogClose(event: LogCloseEvent): void {
+  let entity = new LogClose(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
   entity.tokenId = event.params.tokenId
   entity.owner = event.params.owner
-
-  // Entities can be written to the store with `.save()`
+  entity.amount0 = event.params.amount0
+  entity.amount1 = event.params.amount1
+  entity.fee0 = event.params.fee0
+  entity.fee1 = event.params.fee1
+  entity.token0to1 = event.params.token0to1
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.NftToOwner(...)
-  // - contract.createPosition(...)
-  // - contract.ownerToNftBool(...)
-  // - contract.token0to1(...)
 }
 
-export function handleLogCreate(event: LogCreate): void {}
+export function handleLogCreate(event: LogCreateEvent): void {
+  let entity = new LogCreate(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.tokenId = event.params.tokenId
+  entity.owner = event.params.owner
+  entity.liquidity = event.params.liquidity
+  entity.amount = event.params.amount
+  entity.token0to1 = event.params.token0to1
+  entity.save()
+}
